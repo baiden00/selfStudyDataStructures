27b1b4a897f455631aea94a210c78da0df0da7c8
Charles Edward Baiden <baidenskyress@gmail.com>
2020-05-24 02:14:07 -0400

    Update linkedL.py

--- linkedL.py
+++ linkedL.py
@@ -11,7 +11,7 @@ class LinkedList:
         self.head = Node(value)
 
     #method to insert at the beginning
-    def insertAtStart(self,value):
+    def insert_at_start(self,value):
         #newNode = Node(value)
         '''The above line will create a new node and insert
         it but in this case I have already created the nodes'''
@@ -20,14 +20,14 @@ class LinkedList:
         self.head = value
 
     #traversing through the linkedlist and printing all the elements
-    def printList(self):
+    def print_list(self):
         val = self.head
         while val:
             print(val.value)
             val = val.next
 
     #traversing a LinkedList to search for a value
-    def searchValue(self, toSearch):
+    def search_value(self, toSearch):
         node = self.head
         while node is not None:
             if node.value == toSearch.value:
@@ -37,80 +37,80 @@ class LinkedList:
         return False
 
     #deleting an item from the linkedlist
-    def deleteFromLinkedList(self, toDelete):
+    def delete_from_linked_list(self, to_delete):
         node = self.head
 
         #if the head is to be deleted
-        if node.value == toDelete.value:
+        if node.value == to_delete.value:
             self.head = node.next
 
         #if not the head
         else:
           while node is not None:
-            nextNode = node.next
-            if nextNode.value == toDelete.value:
-              node.next = nextNode.next
+            next_node = node.next
+            if next_node.value == to_delete.value:
+              node.next = next_node.next
               break
             else:
-              node = nextNode
+              node = next_node
 
     #inserting inbetween the linkedlist
-    def insertInBetween(self, afterThisValue, toInsert):
+    def insert_in_between(self, after_this_value, to_insert):
 
         #case where the location is not in the linkedlist
 
-        if not self.searchValue(afterThisValue):
+        if not self.search_value(after_this_value):
             print("The location does not exist in the linkedlist")
             return
 
-        toInsert.next = afterThisValue.next
-        afterThisValue.next = toInsert
+        to_insert.next = after_this_value.next
+        after_this_value.next = to_insert
 
     #to insert at the end
-    def insertAtEnd(self, newdata):
+    def insert_at_end(self,new_data):
 
         if self.head is None:
-            self.head = newdata
+            self.head = new_data
             return
         last = self.head
         while(last.next):
             last = last.next
-        last.next = newdata
+        last.next = new_data
 
 
 
 #creating a new linkedlist
-daysOfWeek = LinkedList()
+days_of_week = LinkedList()
 days = [monday,tuesday,wednesday,thursday,friday]
 
 #inserting all days of the week at the beginning of the node
 #in this case I want to start from monday
 for day in days[::-1]:
-    daysOfWeek.insertAtStart(day)
+    days_of_week.insert_at_start(day)
 
 #this will print the whole linkedlist starting from the last inserted node
-daysOfWeek.printList()
+days_of_week.print_list()
 print("")
 print("")
 
 #Returns "false since charles is not in the linkedlist"
 charles = Node("Charles Baiden")
-print(daysOfWeek.searchValue(charles))
+print(days_of_week.search_value(charles))
 print("")
 print("")
 
 #delete wednesday and print the resulting LinkedList
-daysOfWeek.deleteFromLinkedList(wednesday)
-daysOfWeek.printList()
+days_of_week.delete_from_linked_list(wednesday)
+days_of_week.print_list()
 print("")
 print("")
 
 #insert wednesday back
-daysOfWeek.insertInBetween(tuesday, wednesday)
-daysOfWeek.printList()
+days_of_week.insert_in_between(tuesday, wednesday)
+days_of_week.print_list()
 print("")
 print("")
 
 #insert saturday at the end
-daysOfWeek.insertAtEnd(saturday)
-daysOfWeek.printList()
+days_of_week.insert_at_end(saturday)
+days_of_week.print_list()
